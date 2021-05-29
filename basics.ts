6b67b819922586154e0f71a9d26c136d5acae1fe
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-29 14:24:15 -0500

    ts complex types

--- basics.ts
+++ basics.ts
@@ -2,6 +2,7 @@
 //more complex types: arrays, objects
 //function types, parameters
 //have null and undefined types
+//falback type is any
 
 //primitives
 
@@ -20,3 +21,26 @@ userName = false;
 let isUsingTS: boolean;
 
 isUsingTS = true;
+
+//more complex types
+
+let hobbies: string[]; //an array of strings
+
+hobbies = ["sports", "cooking"];
+
+hobbies.push(12);
+
+let person: {name: string, age: number}; //object typing
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+person = {
+  isEmployee: true;
+}
+
+let people: {
+  name: string, age: number
+}
