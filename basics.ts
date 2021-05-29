dbb2c17f78f935cbf0dd2b269da09c02cb8cd210
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-29 14:20:19 -0500

    ts basics

---
+++ basics.ts
@@ -0,0 +1,22 @@
+//Primitieve: nubmer, string, boolean
+//more complex types: arrays, objects
+//function types, parameters
+//have null and undefined types
+
+//primitives
+
+let age: number;
+
+age = 12;
+
+age = "bob";
+
+let userName: string;
+
+userName = "bob";
+
+userName = false;
+
+let isUsingTS: boolean;
+
+isUsingTS = true;
