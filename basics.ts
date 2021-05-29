a0b506429b561fa19dc2a7138ce182a8702bad0a
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-29 14:38:27 -0500

    generics

--- basics.ts
+++ basics.ts
@@ -59,4 +59,23 @@ let courses: string | number = "react- the complete guide"
 
 courses = 12345
 
+//functions && types
+function add(a: number, b: number): string {
+return a.toString() + b.toString(); 
+}
 
+function printer(value: any) {
+  console.log(value)
+}
+
+//generics
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array]
+  return newArray;
+}
+
+const demoArray = [1,2,3]
+
+const updatedArray = insertAtBeginning(demoArray, -1);  //[-1,1,2,3]
+
+updatedArray[0].split('')
